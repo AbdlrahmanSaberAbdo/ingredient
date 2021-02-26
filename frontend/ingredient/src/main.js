d008cf6d9cdfaaba04947168f161e6d2d6b8f669
AbdlrahmanSaber <abdlrahmansaber652@gmail.com>
2021-02-26 12:21:22 -0800

    Delete frontend/ingredient directory

--- frontend/ingredient/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import Vue from 'vue'
-// Optionally install the BootstrapVue icon components plugin
-
-createApp(App).use(router).mount('#app')
